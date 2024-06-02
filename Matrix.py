05356455fddb53c663b957895bdbb0f7f79473cc
sedthon2440 <166177796+sedthon2440@users.noreply.github.com>
2024-06-02 22:40:12 +0300

    Update Matrix.py

--- Matrix.py
+++ Matrix.py
@@ -40,69 +40,28 @@ from telethon.tl.functions.messages import (
     SendReactionRequest
 )
 
-from telethon import TelegramClient, events, Button
-import asyncio
 
-# بيانات الدخول للبوت
-api_id = 17211426 #  أدخل رقم API ID الخاص بك
-api_hash = '656a097533402eb717ba82298a752177'  # أدخل هاش API الخاص بك
-bot_token = '7359528819:AAFwzmAPxsVZ4piI8CaSxiseNWIs6lbPKHg'  # أدخل توكن البوت
+from telethon import TelegramClient, events
 
-# إنشاء عميل تيلثون
-client = TelegramClient('session_name', api_id, api_hash).start(bot_token=bot_token)
 
-# تعريف الكيبورد مع خيارات الردود
-keyboard = [
-    [Button.inline("رقم هاتفي", data="phone_number"), Button.inline("رمز الدخول", data="auth_code")]
-]
+api_id = 26478228
+api_hash = '7bd0b1761bebea880a084631044ea2bf'
+bot_token = '6563637329:AAGMoZlngSyYkVvvYfCO8luQ1H7gFYACCy4'  
 
-@client.on(events.NewMessage(pattern='/get_api'))
-async def get_api_handler(event):
-    await event.respond("أرسل لي رقم هاتفك.")
 
-@client.on(events.CallbackQuery())
-async def callback_handler(event):
-    data = event.data.decode()
+client = TelegramClient('session_name', api_id, api_hash)
 
-    if data == 'phone_number':
-        await event.edit("أرسل لي رقم هاتفك.")
-    elif data == 'auth_code':
-        await event.edit("أرسل لي رمز الدخول الذي تلقيت.")
+@client.on(events.NewMessage(content_types=['photo']))
+async def handle_new_photo(event):
+    
+    photo_id = event.message.photo.id
+    
+    photo_url = f'https://api.thetellus.io/v1/images/{photo_id}'
 
-@client.on(events.NewMessage(pattern='/start'))
-async def start_handler(event):
-    await event.respond("مرحبا! 👋", buttons=keyboard)
+    await event.reply(f'رابط الصورة: {photo_url}')
 
-@client.on(events.NewMessage(pattern='/get_api'))
-async def get_api_handler(event):
-    await event.respond("أرسل لي رقم هاتفك.")
 
-    # انتظار الرسالة من المستخدم 
-    phone = await client.wait_for(events.NewMessage(from_users=event.sender_id))
-    phone_number = phone.message.text
+client.start(bot_token=bot_token)  
+client.run_until_disconnected()
 
-    #  ارسال رمز الدخول
-    await client.send_code_request(phone_number)
-    await event.respond("أرسل لي رمز الدخول الذي تلقيت.")
-
-    # انتظار رمز الدخول 
-    code = await client.wait_for(events.NewMessage(from_users=event.sender_id))
-    code_message = code.message.text
-
-    try:
-        #  تسجيل الدخول باستخدام رمز الدخول 
-        await client.sign_in(phone_number, code_message)
-        api_id = client.session.api_id
-        api_hash = client.session.api_hash
-
-        #  ارسال api_id + api_hash
-        await event.respond(f"API ID: {api_id}\nAPI Hash: {api_hash}")
-
-    except Exception as e:
-        await event.respond(f"حدث خطأ: {e}")
-
-async def main():
-    await client.start(bot_token=bot_token) # استخدم bot_token عند بدء تشغيل البوت
-    print("البوت جاهز للعمل")
-    await client.run_until_disconnected()
 
